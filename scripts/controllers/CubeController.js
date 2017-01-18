66f08facde6c2c3aeaca505a2e5991f9b581b836
carmoreno <carlosandresmorenovelez@gmail.com>
2017-01-17 19:02:14 -0500

    Comments about functions and variables in services/CubeService.js and controllers/CubeController.js

--- scripts/controllers/CubeController.js
+++ scripts/controllers/CubeController.js
@@ -1,33 +1,66 @@
 /**
 Author: Carlos Andrés Moreno
-Fecha: Enero 
+Fecha: Enero 15 de 2017
+Fecha ultima modificación = Enero 17 de 2017
+Controlador: CubeController
 **/
 var app = angular.module("CubeSummation")
 app.controller('CubeController', ['$scope', 'cubeService', 
 
 	function($scope, cubeService) {
-		$scope.metaData = {}
-		$scope.operations = [{"operation": "UPDATE"}]
-		$scope.optionSelect = ['UPDATE', 'QUERY']
-		$scope.solution = []
 
+		// Este objeto guarda metadatos del problema, como el tamaño del cubo y el numero de tests.
+		$scope.metaData = {}
+		/* En este array se guardan las operacion que se van a realiazar en un test, sus elementos son objetos js
+		este array lucirá así: 
+		[
+		 	{"operation":"UPDATE", "numbers":"1 1 1 45"}, 
+		 	{"operation":"UPDATE", "numbers":"3 3 3 45"},
+		 	{"operation":"QUERY", "numbers":"1 1 1 3 3 3"},
+		 	{"operation":"QUERY", "numbers":"1 1 1 2 2 2"}
+		]
+		Para obtener la cantidad de operaciones que se realiazará, basta con obtener el tamaño del arreglo anterior.
+		/* 
+		$scope.operations = [{"operation": "UPDATE"}]
+		$scope.optionSelect = ['UPDATE', 'QUERY'] // array simple para mostrar las opciones de campo select del formulario
+		$scope.solution = [] // donde se guardará la solución
+
+		/**
+		 * [addOperation Añade una nueva operación que será renderizada en el vista, esta función es
+		 * ejecutada cada vez que se hace click en el boton de "Agregar nueva operación". Por defecto
+		 * será renderizada una operacion UPDATE en la vista.]
+		 */
 		$scope.addOperation = function() {
 			$scope.operations.push({"operation": "UPDATE"})
 		}
+
+		/**
+		 * [removeOperation Remueve la última operación (UPDATE o DELETE) existente en la vista, esta función
+		 *  es ejecutada cada vez que se hace click en el botón de "Remover operación"]
+		 */
 		$scope.removeOperation = function() {
 			$scope.operations.pop()
 		}
 
+		/**
+		 * [resetSolution Establece un arreglo vacío cuando se da por terminada la ejecución de todos los test
+		 * establecidos por el usuario]
+		 */
 		$scope.resetSolution = function() {
 			if($scope.metaData.test < 0) {
 				$scope.solution = []
 			}
 		}
 
+		/**
+		 * [execute Esta función envía los mensajes correspondientes al servicio "CubeService" para que este
+		 * pueda realizar los cálculos pertinentes con los datos que el usuario envió previamente
+		 * desde la vista. Esta función se ejecuta cuando el usuario hace click en el botón "Calcular"]
+		 */
 		$scope.execute = function() {
 			cubeService.init($scope.metaData, $scope.operations, $scope.solution)
 			cubeService.evalOperations()
-			$scope.metaData.test--
+			$scope.metaData.test-- // El número de test disminuye, pues ya se ha ejecutado uno.
 			$scope.resetSolution()
 		}
 
--- scripts/services/CubeService.js
+++ scripts/services/CubeService.js
@@ -1,114 +1,163 @@
+/**
+Author: Carlos Andrés Moreno
+Fecha: Enero 15 de 2017
+Fecha ultima modificación = Enero 17 de 2017
+Servicio: CubeServicie
+**/
 var app = angular.module("CubeSummation")
-app.factory('cubeService', function() {
-	var cubeService = {}
-	var matrix = []
-	var size
-	var operations
-	var solution
+app.service('cubeService', 
 
-	cubeService.init = function(objMetaData, arrayOperations, arraySolution) {
-		size = objMetaData.size
-		operations = arrayOperations
-		solution = arraySolution
-		cubeService.createCube()
-	}
+	function() {
+		var cubeService = {}
+		var matrix = []
+		var size
+		var operations
+		var solution
 
-	cubeService.createCube = function() {
-		try{
-			for (var x = 0; x < size;  x++) {
-				for (var y = 0; y < size;  y++) {
-					for (var z = 0; z < size; z++) {
-						if(!Array.isArray(matrix[x])) {
-							matrix[x] = []
+		/**
+		 * [init   Este método inicializa los valores que serán usados en todo el servicio, recibe los mensajes
+		 * que el CubeController le envia y además, se encarga de hacer el llamado a la creacion del Cubo.]
+		 * @param  {[Objetc]} objMetaData     [Objeto con metadatos de tamaño de cubo y numero de tests. El 
+		 * siguiente es un ejemplo de como luce este objeto: {"test":"2", "size": "4"} ]
+		 * @param  {[Array]} arrayOperations [Array que contiene objetos para las operaciones. EL siguiente
+		 *  es un ejemplo de como luce este array: [{"operation":"UPDATE", "numbers":"1 2 1 34"}, {"operation":"QUERY","numbers":"1 1 1 2 2 2"}] ]
+		 * @param  {[Array]} arraySolution  [Array que va a almacenar las soluciones para posteriormente mostrarlas
+		 *  en la vista]
+		 */
+		cubeService.init = function(objMetaData, arrayOperations, arraySolution) {
+			size = objMetaData.size
+			operations = arrayOperations
+			solution = arraySolution
+			cubeService.createCube()
+		}
+
+		/**
+		 * [createCube Crea una matrix en 3d (El Cubo) e inicializa todas sus celdas con el valor de
+		 * cero]
+		 */
+		cubeService.createCube = function() {
+			try{
+				for (var x = 0; x < size;  x++) {
+					for (var y = 0; y < size;  y++) {
+						for (var z = 0; z < size; z++) {
+							if(!Array.isArray(matrix[x])) {
+								matrix[x] = []
+							}
+							if(!Array.isArray(matrix[x][y])) {
+								matrix[x][y] = []
+							}
+							if(!Array.isArray(matrix[x][y])) {
+								matrix[x][y][z] = []
+							}
+							matrix[x][y][z] = 0
 						}
-						if(!Array.isArray(matrix[x][y])) {
-							matrix[x][y] = []
-						}
-						if(!Array.isArray(matrix[x][y])) {
-							matrix[x][y][z] = []
-						}
-						matrix[x][y][z] = 0
+					}
+				}
+			}//Fin try
+			catch(error){
+				alert("¡Vaya!, un error ha ocurrido: "+error)
+			}
+		}//Fin createCube
+
+		/**
+		 * [evalOperations Esta función se encarga de verificar cuales datos se envian a las funciones update
+		 * o query, puesto que la información que necesita cada función es diferente (e.g. update solo necesita
+		 * cuatro númeors, mientras que query necesita 6)]
+		 */
+		cubeService.evalOperations = function() {
+			try{
+				for (var i in operations) {
+					if(operations[i].operation == "UPDATE") {
+						cubeService.update(operations[i].numbers)
+					}
+					else {
+						cubeService.query(operations[i].numbers)
 					}
 				}
 			}
-		}//Fin try
-		catch(error){
-			alert("¡Vaya!, un error ha ocurrido: "+error)
+			catch(error){
+				alert("¡Vaya!, un error ha ocurrido: "+error)
+			}
 		}
-	}//Fin createCube
 
-	cubeService.evalOperations = function() {
-		try{
-			for (var i in operations) {
-				if(operations[i].operation == "UPDATE") {
-					cubeService.update(operations[i].numbers)
+		/**
+		 * [convertNumbers Función auxiliar que se encarga de convertir una cadena de numeros
+		 * seaparados por un espacio, en un arreglo de numeros enteros. Esto con el fin de utilizar
+		 * los valores más adelante para realiazar las operaciones de UPDATE y QUERY]
+		 * @param  {[string]} numbers [Cadena de números separados por un espacio]
+		 * @return {[Array]}         [Arreglo de numeros enteros]
+		 */
+		cubeService.convertNumbers = function(numbers) {
+			try {
+				var arrayNumbers = numbers.split(' ').map(Number)
+				for (var i in numbers) {
+					arrayNumbers.push(parseInt(numbers[i]))
+				}
+				return arrayNumbers
+			} 
+			catch(error) {
+				alert("¡Vaya!, un error ha ocurrido: "+error)
+			}
+		}
+
+		/**
+		 * [update Esta función se encarga de realizar la operacion de UPDATE dado una cadena de numeros.
+		 * En esta cadena viene la información de las posiciones y el valor a insertar. Por ejemplo, la cadena
+		 * '2 3 2 34' quiere decir que en la posicion (2,3,2) se va a insertar el valor 34]
+		 * @param  {[string]} numbers [Cadena de números]
+		 */
+		cubeService.update = function(numbers) {
+			try{
+				var intNumbers = cubeService.convertNumbers(numbers) //Convertimos la cadena a un array de enteros
+				var x = intNumbers[0] - 1
+				var y = intNumbers[1] - 1
+				var z = intNumbers[2] - 1
+				var W = intNumbers[3] // Valor a insertar
+				// El problema habla de una restricción para el valor a insertar, por esa razón se verifica.
+				if(W >= Math.pow(-10, 9) && W <= Math.pow(10, 9)) {
+					matrix[x][y][z] = W 
 				}
 				else {
-					cubeService.query(operations[i].numbers)
+					alert("La cantidad a insertar no está en los límites establecidos")
 				}
 			}
-		}
-		catch(error){
-			alert("¡Vaya!, un error ha ocurrido: "+error)
-		}
-	}
-
-	cubeService.convertNumbers = function(numbers) {
-		try {
-			var arrayNumbers = numbers.split(' ').map(Number)
-			for (var i in numbers) {
-				arrayNumbers.push(parseInt(numbers[i]))
-			}
-			return arrayNumbers
-		} 
-		catch(error) {
-			alert("¡Vaya!, un error ha ocurrido: "+error)
-		}
-	}
-
-	cubeService.update = function(numbers) {
-		try{
-			var intNumbers = cubeService.convertNumbers(numbers)
-			var x = intNumbers[0] - 1
-			var y = intNumbers[1] - 1
-			var z = intNumbers[2] - 1
-			var W = intNumbers[3]
-			if(W >= Math.pow(-10, 9) && W <= Math.pow(10, 9)) {
-				matrix[x][y][z] = W 
-			}
-			else {
-				alert("La cantidad a insertar no está en los límites establecidos")
+			catch(error) {
+				alert("¡Vaya!, un error ha ocurrido: "+error)
 			}
 		}
-		catch(error) {
-			alert("¡Vaya!, un error ha ocurrido: "+error)
-		}
-	}
 
-	cubeService.query = function(numbers) {
-		try{
-			var intNumbers = cubeService.convertNumbers(numbers)
-			var suma = 0
-			var x1 = intNumbers[0] - 1
-			var y1 = intNumbers[1] - 1
-			var z1 = intNumbers[2] - 1
-			var	x2 = intNumbers[3] - 1
-			var y2 = intNumbers[4] - 1
-			var z2 = intNumbers[5] - 1
+		/**
+		 * [query Esta función se encarga de realizar la operacion QUERY dada una cadena de números, en esta
+		 * cadena está la información de las posiciones. Por ejemplo, la cadena '1 1 1 3 3 3' quiere decir
+		 * que se hará la suma de las posiciones (1,1,1);(2,2,2) y (3,3,3)]
+		 * @param  {[string]} numbers [cadena de números]
+		           
+		 */
+		cubeService.query = function(numbers) {
+			try{
+				var intNumbers = cubeService.convertNumbers(numbers)
+				var suma = 0
+				// Establecemos las posiciones
+				var x1 = intNumbers[0] - 1
+				var y1 = intNumbers[1] - 1
+				var z1 = intNumbers[2] - 1
+				var	x2 = intNumbers[3] - 1
+				var y2 = intNumbers[4] - 1
+				var z2 = intNumbers[5] - 1
 
-			for (var x = x1; x <= x2; x++) {
-				for (var y = y1; y <= y2; y++) {
-					for (var z = z1; z <= z2; z++) {
-						suma += matrix[x][y][z]
+				for (var x = x1; x <= x2; x++) {
+					for (var y = y1; y <= y2; y++) {
+						for (var z = z1; z <= z2; z++) {
+							suma += matrix[x][y][z]
+						}
 					}
 				}
+				solution.push(suma)// Guardamos en un arreglo las sumas obtenidas
 			}
-			solution.push(suma)
+			catch(error){
+				alert("¡Vaya!, un error ha ocurrido: "+error)
+			}
 		}
-		catch(error){
-			alert("¡Vaya!, un error ha ocurrido: "+error)
-		}
-	}
 
-	return cubeService
+		return cubeService
 })
